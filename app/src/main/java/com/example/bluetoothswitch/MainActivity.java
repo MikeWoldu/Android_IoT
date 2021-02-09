ee8e5c66007bdb13e6dc03e0e276633f37f43dda
Konstantinos <61021816+uppercaseGuy091@users.noreply.github.com>
2021-02-09 13:28:15 +0100

    i added a comment

--- app/src/main/java/com/example/bluetoothswitch/MainActivity.java
+++ app/src/main/java/com/example/bluetoothswitch/MainActivity.java
@@ -31,6 +31,7 @@ import static android.content.ContentValues.TAG;
 
 //this is a test to see if my branch and account work
 //new test test test
+//new comment from impleneting Bluetooth bracnh
 
 
 public class MainActivity extends AppCompatActivity {
